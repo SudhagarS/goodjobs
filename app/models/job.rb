839add8d42f99d68af01a2bee89c9cbaa05128bc
SudhagarS <sudhagar@isudhagar.in>
2016-08-02 18:08:39 +0530

    Increment per page value

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,7 +9,7 @@ class Job < ActiveRecord::Base
   validates :title, :company_name, :location, presence: true
 
   def self.search(params)
-    tire.search(load:true, page: params[:page], per_page: 5) do
+    tire.search(load:true, page: params[:page], per_page: 10) do
       query { string params[:query], default_operator: 'AND' } if params[:query].present?
       sort { by :created_at, "desc" } if params[:query].blank?
     end
